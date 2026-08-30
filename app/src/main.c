8d64bd92be8dba24bcbe6afd02eebcfa0e354fc5
Ilya Tanko <ti1998.1998@gmail.com>
2026-08-30 02:08:33 +0200

    Task 2 - mutex sync

--- app/src/main.c
+++ app/src/main.c
@@ -3,64 +3,60 @@
 
 #include <stdbool.h>
 
-LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(demo, LOG_LEVEL_INF);
 
 #define STACK_SIZE 1024
 
-static const int PRIO_COOP = -1;
-static const int PRIO_HIGH = 3;
-static const int PRIO_MEDIUM = 5;
-static const int PRIO_LOW = 7;
+static const int PRIO = 5;
 
-void t_low_fn(void* p1, void* p2, void* p3) {
-    LOG_INF("Thread Low started");
-    static int count = 0;
+// Each thread increments this many times
+static const uint32_t INCREMENTS = 500000;
 
-    while (true) {
-        LOG_INF("Thread Low count: %d, tick: %u", count++, k_uptime_get_32());
-        k_msleep(300);
-    }
-}
+// Shared state
+static volatile uint32_t counter;
 
-void t_medium_fn(void* p1, void* p2, void* p3) {
-    LOG_INF("Thread Medium started");
-    static int count = 0;
+static struct k_sem done_sem;
 
-    while (true) {
-        LOG_INF("Thread Medium count: %d, tick: %u", count++, k_uptime_get_32());
-        k_msleep(200);
-    }
-}
+static K_MUTEX_DEFINE(counter_mutex);
 
-void t_high_fn(void* p1, void* p2, void* p3) {
-    LOG_INF("Thread High started");
-    static int count = 0;
+void worker_fn(void* p1, void* p2, void* p3) {
+    for (int i = 0; i < INCREMENTS; i++) {
+        k_mutex_lock(&counter_mutex, K_FOREVER);
+        counter++;
 
-    while (true) {
-        LOG_INF("Thread High count: %d, tick: %u", count++, k_uptime_get_32());
-        k_msleep(100);
-    }
-}
-
-void t_coop_fn(void* p1, void* p2, void* p3) {
-    LOG_INF("Thread Coop started");
-
-    while (true) {
-        static const uint8_t ITERATIONS = 5;
-        for (int i = 0; i < ITERATIONS; i++) {
-            k_busy_wait(500 * 1000); // 500 ms
-            LOG_INF("Thread Coop step %d/%d tick=%u", i + 1, ITERATIONS, k_uptime_get_32());
+        if (counter % 100000 == 0) {
+            LOG_INF("Counter: %u", counter);
         }
-        k_yield();
+        k_mutex_unlock(&counter_mutex);
     }
+
+    const char* name = k_thread_name_get(k_current_get());
+    LOG_INF("[%s] finished", name);
+    k_sem_give(&done_sem);
 }
 
-K_THREAD_DEFINE(thread_low, STACK_SIZE, t_low_fn, NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_medium, STACK_SIZE, t_medium_fn, NULL, NULL, NULL, PRIO_MEDIUM, 0, 0);
-K_THREAD_DEFINE(thread_high, STACK_SIZE, t_high_fn, NULL, NULL, NULL, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(thread_coop, STACK_SIZE, t_coop_fn, NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(worker_a, STACK_SIZE, worker_fn, NULL, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(worker_b, STACK_SIZE, worker_fn, NULL, NULL, NULL, PRIO, 0, 0);
 
 int main(void) {
+    k_sem_init(&done_sem, 0, 2);
+    int64_t time = k_uptime_get();
+
+    LOG_INF("=== Mutex Protection Demo ===");
+    LOG_INF("Expected final value: %d", INCREMENTS * 2);
+
+    // Wait for both workers to complete
+    k_sem_take(&done_sem, K_FOREVER);
+    k_sem_take(&done_sem, K_FOREVER);
+
+    LOG_INF("Actual final value: %u", counter);
+
+    if (counter == (INCREMENTS * 2)) {
+        LOG_WRN("No race!");
+    } else {
+        LOG_ERR("Race condition confirmed: lost %d updates", (INCREMENTS * 2) - counter);
+    }
+    LOG_INF("Execution time: %lld ms", k_uptime_delta(&time));
+
     return 0;
 }
-
